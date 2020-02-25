63c058a5fb45f93b5107541eb63ecec8faf9eea8
LAPTOP-M21HOF1S\赵志杰 <1420800094@qq.com>
2020-02-25 16:13:04 +0800

    add product apis

--- jcart-administration-back/src/main/java/com/zhaozhijie/jcartadministrationback/controller/ProductController.java
+++ jcart-administration-back/src/main/java/com/zhaozhijie/jcartadministrationback/controller/ProductController.java
@@ -1,12 +1,12 @@
 package com.zhaozhijie.jcartadministrationback.controller;
 
+import com.zhaozhijie.jcartadministrationback.dto.in.ProductCreateInDTO;
 import com.zhaozhijie.jcartadministrationback.dto.in.ProductSearchInDTO;
+import com.zhaozhijie.jcartadministrationback.dto.in.ProductUpdateInDTO;
 import com.zhaozhijie.jcartadministrationback.dto.out.PageOutDTO;
 import com.zhaozhijie.jcartadministrationback.dto.out.ProductListOutDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.zhaozhijie.jcartadministrationback.dto.out.ProductShowOutDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -18,4 +18,18 @@ public class ProductController {
         return null;
     }
 
+    @GetMapping("/getById")
+    public ProductShowOutDTO getById(@RequestParam Integer productId){
+        return null;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody ProductCreateInDTO productCreateInDTO){
+        return null;
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody ProductUpdateInDTO productUpdateInDTO){
+
+    }
 }
