c8d329d73f88bb65233e00894d893c418641dd87
LAPTOP-M21HOF1S\赵志杰 <1420800094@qq.com>
2020-02-25 18:00:46 +0800

    add store address apis

--- jcart-store-back/src/main/java/com/zhaozhijie/jcartstoreback/controller/AddressController.java
+++ jcart-store-back/src/main/java/com/zhaozhijie/jcartstoreback/controller/AddressController.java
@@ -1,9 +1,31 @@
 package com.zhaozhijie.jcartstoreback.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.zhaozhijie.jcartstoreback.dto.in.AddressCreateInDTO;
+import com.zhaozhijie.jcartstoreback.dto.in.AddressUpdateInDTO;
+import com.zhaozhijie.jcartstoreback.dto.out.AddressListOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/address")
 public class AddressController {
+
+    @GetMapping("/getAddressByCustomerId")
+    public List<AddressListOutDTO> getAddressByCustomerId(@RequestAttribute Integer customerId){
+        return null;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody AddressCreateInDTO addressCreateInDTO,
+                          @RequestAttribute Integer customerId){
+        return null;
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody AddressUpdateInDTO addressUpdateInDTO,
+                       @RequestAttribute Integer customerId){
+
+    }
+
 }
