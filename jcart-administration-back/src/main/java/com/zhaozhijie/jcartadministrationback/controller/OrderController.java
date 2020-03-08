8f7e5c6471ea9e9443d317e6ebe3ad9bbec359b0
LAPTOP-M21HOF1S\赵志杰 <1420800094@qq.com>
2020-03-08 17:12:36 +0800

    add administration address index page

--- jcart-administration-back/src/main/java/com/zhaozhijie/jcartadministrationback/controller/OrderController.java
+++ jcart-administration-back/src/main/java/com/zhaozhijie/jcartadministrationback/controller/OrderController.java
@@ -5,13 +5,11 @@ import com.zhaozhijie.jcartadministrationback.dto.in.OrderSearchInDTO;
 import com.zhaozhijie.jcartadministrationback.dto.out.*;
 import com.zhaozhijie.jcartadministrationback.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
+@CrossOrigin
 public class OrderController {
 
     @Autowired
